912d4b426fea4c30e463f09e9a02bce972923d9b
YuiHironaka <81809@DESKTOP-I27R6NU>
2024-07-27 17:17:28 +0900

    課題修正・if構文の順番を変更（iの値が3で割り切れ、かつ5で割り切れるときの条件を一番はじめへ）

--- src/Review02.java
+++ src/Review02.java
@@ -3,29 +3,29 @@ public class Review02 {
 
     public static void main(String[] args) {
      // iの値を0から100より小さい間、1ずつカウントアップして繰り返す
-        for (int i = 1; i < 100; i++) {
-            
+        for (int i = 1; i <= 100; i++) {
+
+         // iの値が5で割り切れるとき
+         if (i % 3 == 0 && i % 5 == 0) {
+              // FizzBuzz と表示する
+              System.out.println("FizzBuzz");
+
          // iの値が2で割り切れるとき
-            if (i % 3 == 0) {
+          } else if (i % 3 == 0) {
                 // Fizz と表示する
                 System.out.println("Fizz");
-  
+
          // iの値が5で割り切れるとき
           } else if (i % 5 == 0) {
                 // Buzz と表示する
                 System.out.println("Buzz");
-
-         // iの値が5で割り切れるとき
-          } else if (i % 3 == 0 && i % 5 == 0) {
-                // FizzBuzz と表示する
-                System.out.println("FizzBuzz");
 
          // それ以外の数
           } else {
                 // その数をそのまま表示する
                 System.out.println(i);
           }
-       
+
         }
     }
 
